0ade3e63505eb58949f34c7b38dc4a99760227a0
SamriddhiMishra <samriddhimishra510@gmail.com>
2025-08-26 23:53:53 +0100

    Bonus 2 code added

--- Practice Python Exercise 5.py
+++ Practice Python Exercise 5.py
@@ -32,3 +32,8 @@ for item in list_1:
         intersection_of_lists.append(item)
 
 print(list_1,list_2, intersection_of_lists, sep="\n")
+
+#Bonus 2
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+print(list({item for item in a if item in b}))
